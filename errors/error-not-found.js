2e197097155e59b200001768757a9c49bb096616
Sergey Nesterovich <blooker@yandex.ru>
2022-10-27 21:01:06 +0300

    add readme

--- errors/error-not-found.js
+++ errors/error-not-found.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const NotFoundError = require('../errors/not-found-err');
+const NotFoundError = require('./not-found-err');
 
 router.all('*', (req, res, next) => {
   next(new NotFoundError('Такой страницы не существует'));
